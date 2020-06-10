5899aeb955d24cdb0b337ab3982e3df4b53f87d1
nisaprmst <nisaprmst@gmail.com>
2020-06-10 23:20:14 +0700

    homepage

---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,19 @@
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
+
+
